c5b4c29c8ea903b5a8d4e8e90901a395a038f69d
!pancak <molenshark@gmail.com>
2026-05-17 19:27:38 +0700

    Add files via upload

---
+++ configd/matugen/templates/hyprland.lua
@@ -0,0 +1,7 @@
+return {
+    border_active = "{{ colors.primary.default.hex }}",
+    border_inactive = "{{ colors.surface.default.hex }}",
+    shadow = "{{ colors.shadow.default.hex }}",
+    background = "{{ colors.background.default.hex }}",
+    foreground = "{{ colors.on_background.default.hex }}",
+}
